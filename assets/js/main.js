06136bf22c4de0645774ef43c3109bbd5a1d5783
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-05-20 19:09:27 -0500

    [ADD] header mobile and desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
+/* Open Menu Mobile */
+const menuImg = document.querySelector('.icon-menu')
+const menuContainer = document.querySelector('.menu__container')
 
+const openMenu = () => {
+  menuContainer.style.right = 0
+}
+menuImg.addEventListener('click', openMenu)
+
+/* Close menu mobile */
+const iconClose = document.querySelector('.icon-close')
+
+const closeMenu = () => {
+  menuContainer.style.right = '-100%'
+}
+iconClose.addEventListener('click', closeMenu)
+
+/* Expand and Collapsed menu */
+const iconsArrowDown = document.querySelectorAll('.toggle-arrow')
+
+const toggleMenu = (event) => {
+  const iconArrow = event.target.nextElementSibling
+  const list = iconArrow.nextElementSibling
+  iconArrow.classList.toggle('arrow-down--active')
+  list.style.display === 'flex' ? (list.style.display = 'none') : (list.style.display = 'flex')
+}
+
+iconsArrowDown.forEach((item) => item.addEventListener('click', toggleMenu))
